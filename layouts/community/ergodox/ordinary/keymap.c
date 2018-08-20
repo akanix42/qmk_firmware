0610838369e01d94992b792e665c9aa52c4d688b
Nathan Reid <nathan-code@nathantreid.com>
2018-08-20 14:25:32 -0500

    add to base option on fps layer

--- layouts/community/ergodox/ordinary/keymap.c
+++ layouts/community/ergodox/ordinary/keymap.c
@@ -279,7 +279,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [FPS] = LAYOUT_ergodox(
 // left hand
- F(LSpec)  ,KC_1           ,KC_2   ,KC_3   ,KC_4  ,KC_5  ,KC_ESC
+ TO(BASE)  ,KC_1           ,KC_2   ,KC_3   ,KC_4  ,KC_5  ,KC_ESC
 ,KC_TAB    ,KC_Q           ,KC_W   ,KC_E   ,KC_R  ,KC_T  ,KC_Y
 ,KC_LSFT   ,KC_A           ,KC_S   ,KC_D   ,KC_F  ,KC_G
 ,KC_LCTL   ,KC_Z           ,KC_X   ,KC_C   ,KC_V  ,KC_B  ,LSFT(KC_TAB)
