d524a8e0a05013e8315c4acb4c8a4ea477d101e2
Nathan Reid <nathan-code@nathantreid.com>
2018-08-20 17:18:07 -0500

    move shift back down

--- layouts/community/ergodox/ordinary/keymap.c
+++ layouts/community/ergodox/ordinary/keymap.c
@@ -282,7 +282,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  TO(BASE)  ,KC_1           ,KC_2   ,KC_3   ,KC_4  ,KC_5  ,KC_ESC
 ,KC_TAB    ,KC_Q           ,KC_W   ,KC_E   ,KC_R  ,KC_T  ,KC_Y
 ,KC_LSFT   ,KC_A           ,KC_S   ,KC_D   ,KC_F  ,KC_G
-,KC_LCTL   ,KC_Z           ,KC_X   ,KC_C   ,KC_V  ,KC_B  ,LSFT(KC_TAB)
+,KC_LSFT   ,KC_Z           ,KC_X   ,KC_C   ,KC_V  ,KC_B  ,LSFT(KC_TAB)
 ,KC_LCTL   ,MEH_T(KC_NO)   ,MO(RBASE),KC_LALT,KC_LGUI
                                          ,KC_HOME,KC_END
                                                  ,KC_PGUP
