c5aea3dde2f376566f068f69b3a83e4862818137
Nathan Reid <nathan-code@nathantreid.com>
2018-08-21 08:02:53 -0500

    fix typo

--- layouts/community/ergodox/ordinary/keymap.c
+++ layouts/community/ergodox/ordinary/keymap.c
@@ -324,7 +324,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // left hand
  TO(BASE)  ,KC_F1          ,KC_F2  ,KC_F3  ,KC_F4 ,KC_F5 ,KC_ESC
 ,KC_TAB    ,KC_Q           ,KC_W   ,KC_E   ,KC_R  ,KC_T  ,KC_LBRC
-,kC_NO     ,KC_A           ,KC_S   ,KC_D   ,KC_F  ,KC_G
+,KC_NO     ,KC_A           ,KC_S   ,KC_D   ,KC_F  ,KC_G
 ,KC_LSFT   ,KC_Z           ,KC_X   ,KC_C   ,KC_V  ,KC_B  ,LSFT(KC_TAB)
 ,KC_LCTL   ,MEH_T(KC_NO)   ,MO(RBASE),KC_LALT,KC_LGUI
                                          ,KC_HOME,KC_END
