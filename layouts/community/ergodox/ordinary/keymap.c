890be08011ccf05796a7d4600db997f82f5c5c62
Nathan Reid <nathan-code@nathantreid.com>
2018-09-06 17:43:17 -0500

    replace right hand layer keys with their basic keystrokes

--- layouts/community/ergodox/ordinary/keymap.c
+++ layouts/community/ergodox/ordinary/keymap.c
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  ,KC_PGUP
                                  ,KC_BSPC,KC_DEL ,KC_PGDN
                                                                   // right hand
-                                                                 ,KC_MINS ,KC_6 ,KC_7           ,KC_8   ,KC_9   ,KC_0             ,F(RSpec)
-                                                                 ,KC_RBRC ,KC_Y ,KC_U           ,KC_I   ,KC_O   ,KC_P             ,F(RMdia)
-                                                                          ,KC_H ,KC_J           ,KC_K   ,KC_L   ,KC_SCLN          ,F(RSymb)
+                                                                 ,KC_MINS ,KC_6 ,KC_7           ,KC_8   ,KC_9   ,KC_0             ,KC_EQL
+                                                                 ,KC_RBRC ,KC_Y ,KC_U           ,KC_I   ,KC_O   ,KC_P             ,KC_BSLASH
+                                                                          ,KC_H ,KC_J           ,KC_K   ,KC_L   ,KC_SCLN          ,KC_QUOTE
                                                                  ,KC_TAB  ,KC_N ,KC_M           ,KC_COMM,KC_DOT ,KC_SLSH          ,KC_RSFT
                                                                                 ,KC_RGUI        ,KC_RALT,MO(RBASE),KC_MEH           ,KC_RCTL
                                                                  ,KC_LEFT ,KC_RGHT
