6609b2f9e016dfadf86cd9811936bad799f7d397
Nathan Reid <nathan-code@nathantreid.com>
2018-09-26 09:14:05 -0500

    fix function keys and add pause/break key

--- layouts/community/ergodox/ordinary/keymap.c
+++ layouts/community/ergodox/ordinary/keymap.c
@@ -304,7 +304,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,------------------------------------------------------.       ,------------------------------------------------------.
 *  | TO BASE    |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |       |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |    F11     |
  * |------------+------+------+------+------+-------------|       |------+------+------+------+------+------+------------|
- * | Tab        |  F7  |  F8  |  F9  |  F10 |  F11 | F12  |       |      |  F1  |  F2  |  F3  |  F4  |  F12 |            |
+ * | Tab        |  F7  |  F8  |  F9  |  F10 |  F11 | F12  |       |      |  F1  |  F2  |  F3  |  F4  |  F12 | Pause/Break|
  * |------------+------+------+------+------+------|      |       |      |------+------+------+------+------+------------|
  * | Symbol     |   A  |   S  |   D  |   F  |   G  |------|       |------|   H  |   J  |   K  |   L  |   ;  | '"  Symbol |
  * |------------+------+------+------+------+------|Shift |       | Tab  |------+------+------+------+------+------------|
@@ -322,8 +322,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [FUNCTION] = LAYOUT_ergodox(
 // left hand
- TO(BASE)  ,KC_F1          ,KC_F2  ,KC_F3  ,KC_F4 ,KC_F5 ,KC_ESC
-,KC_TAB    ,KC_Q           ,KC_W   ,KC_E   ,KC_R  ,KC_T  ,KC_LBRC
+ TO(BASE)  ,KC_F1          ,KC_F2  ,KC_F3  ,KC_F4 ,KC_F5 ,KC_F6
+,KC_TAB    ,KC_F7          ,KC_F8  ,KC_F9  ,KC_F10,KC_F11,KC_F12
 ,KC_NO     ,KC_A           ,KC_S   ,KC_D   ,KC_F  ,KC_G
 ,KC_LSFT   ,KC_Z           ,KC_X   ,KC_C   ,KC_V  ,KC_B  ,LSFT(KC_TAB)
 ,KC_LCTL   ,MEH_T(KC_NO)   ,MO(RBASE),KC_LALT,KC_LGUI
@@ -331,8 +331,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  ,KC_PGUP
                                  ,KC_BSPC,KC_DEL ,KC_PGDN
                                                                   // right hand
-                                                                 ,KC_MINS ,KC_F6 ,KC_F7           ,KC_F8   ,KC_F9   ,KC_F10             ,F(RSpec)
-                                                                 ,KC_RBRC ,KC_Y ,KC_U           ,KC_I   ,KC_O   ,KC_P             ,F(RMdia)
+                                                                 ,KC_F5   ,KC_F6,KC_F7          ,KC_F8  ,KC_F9  ,KC_F10           ,KC_F11
+                                                                 ,KC_RBRC ,KC_F1,KC_F2          ,KC_F3  ,KC_F4  ,KC_F12           ,KC_PAUSE
                                                                           ,KC_H ,KC_J           ,KC_K   ,KC_L   ,KC_SCLN          ,F(RSymb)
                                                                  ,KC_TAB  ,KC_N ,KC_M           ,KC_COMM,KC_DOT ,KC_SLSH          ,KC_RSFT
                                                                                 ,KC_RGUI        ,KC_RALT,MO(RBASE),KC_MEH           ,KC_RCTL
